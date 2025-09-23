50f25b9b138bb50cccb71af0406378cef8f4e3d6
MatCelis <91212536+Msilva-debug@users.noreply.github.com>
2025-09-23 12:19:04 -0500

    Frontend - Pokemon
    Modificacion para responsive

--- Frontend-Pokemon/src/app/shared/components/navbar/navbar.ts
+++ Frontend-Pokemon/src/app/shared/components/navbar/navbar.ts
@@ -1,16 +1,22 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { routes } from '../../../app.routes';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'shared-navbar',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.html',
   styles: ``,
 })
 export class Navbar {
+  public stateMenu = signal<boolean>(true);
   public routes = routes
     .map((route) => route.children ?? [])
     .flat()
     .filter((route) => route.data?.['showNavbar']);
+
+  public toggleMenu = () => {
+    this.stateMenu.update((state) => !state);
+  };
 }
