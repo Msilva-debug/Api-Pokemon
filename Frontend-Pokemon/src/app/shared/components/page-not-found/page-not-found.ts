24bbe416e211118769a1f16aa0fc19624cd9b850
MatCelis <91212536+Msilva-debug@users.noreply.github.com>
2025-10-02 14:37:38 -0500

    Frontend - Pokemon
    Responsive

--- Frontend-Pokemon/src/app/shared/components/page-not-found/page-not-found.ts
+++ Frontend-Pokemon/src/app/shared/components/page-not-found/page-not-found.ts
@@ -6,10 +6,12 @@ import { RouterModule } from '@angular/router';
   imports: [RouterModule],
   template: `
     <div
-      class="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-yellow-200 to-red-300 text-gray-800"
+      class="text-center flex flex-col items-center justify-center h-screen bg-gradient-to-b from-yellow-200 to-red-300 text-gray-800"
     >
-      <h1 class="text-9xl font-extrabold tracking-widest text-red-600">404</h1>
-      <div class="bg-red-600 px-2 text-sm rounded rotate-12 absolute">
+      <h1 class="text-9xl font-extrabold  text-primary">404</h1>
+      <div
+        class="bg-primary px-2 text-secondary text-[10px] md:text-sm rounded rotate-12 absolute"
+      >
         Página no encontrada
       </div>
 
@@ -19,17 +21,16 @@ import { RouterModule } from '@angular/router';
           alt="Pikachu"
           class="w-32 h-32 animate-bounce"
         />
-        <p class="mt-4 text-lg font-semibold">¡Pikachu está perdido!</p>
-        <p class="text-sm text-gray-600">
-          Parece que intentaste usar un
-          <span class="font-bold">Pokéball</span> en una ruta inexistente.
+        <p class="mt-4 text-secondary text-lg font-semibold">¡Pikachu está perdido!</p>
+        <p class="text-[10px] md:text-sm text-secondary text-center">
+          Parece que intentaste usar un Pokéball en una ruta inexistente.
         </p>
       </div>
 
       <div class="mt-8">
         <a
           routerLink="/"
-          class="px-6 py-3 rounded-xl bg-yellow-400 hover:bg-yellow-500 text-gray-900 font-bold shadow-lg transition duration-300"
+          class="px-6 py-3 rounded-xl bg-primary hover:bg-yellow-500 text-secondary font-bold shadow-lg transition duration-300"
         >
           ⬅️ Volver al inicio
         </a>
